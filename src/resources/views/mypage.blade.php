2406f7f155b7c8e1d0c547514ac8143b3a2f39c0
y.mutsumi <arkmtm12@icloud.com>
2024-11-06 14:54:20 +0900

    “マイページ→予約情報取得の実装”

--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -2,6 +2,8 @@
 
 @section('css')
     <link rel="stylesheet" href="{{ asset('css/mypage.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
 @endsection
 
 @section('content')
@@ -9,26 +11,32 @@
 <body>
     <main>
         {{-- ログインしているユーザー名を表示 --}}
-        <div class="header__wrap">
-            <p class="header__text">{{ $user->name }}さん</p>
-        </div>
-
+        <div class="container">
+            <div class="header__wrap">
+                <h1 class="header__text">{{ $user->name }}さん</h1>
+            </div>
         {{-- 予約情報の表示 --}}
             <h2>予約状況</h2>
             @if ($reservations->isEmpty())
                 <p>予約はありません。</p>
             @else
-            <div class="reservations">
-                @foreach ($reservations as $reservation)
-                    <div class="reservation-card">
-                        <p>Shop{{ $reservation->restaurant->name}}</p>
-                        <p>Date{{ $reservation->reservation_date }}</p>
-                        <p>Time{{ $reservation->reservation_time }}</p>
-                        <p>Number{{ $reservation->number_of_people }}人</p>
-                    </div>
-                @endforeach
-            </div>
+                <div class="reservations">
+                    @foreach ($reservations as $reservation)
+                        <div class="reservation-card">
+                            <div class="reservation-number">
+                                <i class="fas fa-clock"></i>
+                                <p>予約{{ $loop->iteration }}</p>
+                            </div> <!-- 時計アイコンと予約番号の表示 -->
+
+                            <p><span class="label">Shop</span> <span class="data-shop">{{ $reservation->restaurant->name }}</span></p>
+                            <p><span class="label">Date</span> <span class="data">{{ $reservation->reservation_date }}</span></p>
+                            <p><span class="label">Time</span> <span class="data">{{ $reservation->reservation_time }}</span></p>
+                            <p><span class="label">Number</span> <span class="data-number">{{ $reservation->number_of_people }}人</span></p>
+                        </div>
+                    @endforeach
+                </div>
             @endif
+        </div>
     </main>
 </body>
 
