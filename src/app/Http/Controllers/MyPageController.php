b99d3ce11d77ec03ddd0db3ddccabcc23c7f9237
y.mutsumi <arkmtm12@icloud.com>
2024-11-13 18:35:58 +0900

    “マイページの修正”

--- src/app/Http/Controllers/MyPageController.php
+++ src/app/Http/Controllers/MyPageController.php
@@ -2,20 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use App\Models\Restaurant;
 use App\Models\Reservation;
+use App\Models\Favorite;
+use Illuminate\Support\Facades\Auth;
 
 class MyPageController extends Controller
 {
     public function index()
     {
         $user = Auth::user();
+        $restaurants = Restaurant::all();
 
         // ユーザーの予約情報を取得
         $reservations = Reservation::where('user_id', $user->id)->get();
 
+        // ユーザーのお気に入り店舗を取得
+        $favorites = Favorite::where('user_id', $user->id)->get();
+
         // ビューに変数を渡す
-        return view('mypage', compact('user', 'reservations'));
+        return view('mypage', compact('user', 'restaurants', 'reservations', 'favorites'));
     }
 }
